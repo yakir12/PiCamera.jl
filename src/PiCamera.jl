5ea4427053e174a49fe548cfa48378bf66f78813
yakir12 <12.yakir@gmail.com>
2019-11-22 10:15:04 +0100

    sync

--- src/PiCamera.jl
+++ src/PiCamera.jl
@@ -2,34 +2,24 @@ module PiCamera
 
 export preview
 
-using VideoIO, ImageView, Dates
+using VideoIO, ImageView
 
 function preview()
     cam = VideoIO.opencamera()
     img = read(cam)
     i = imshow(img)
+        sleep(0)
     canvas = i["gui"]["canvas"]
-    start = now()
-    while now() - start ≤ Minute(1)
+for i in 1:50
+        img = read(cam)
+	@show i
+end
+    @async while true
         img = read(cam)
         imshow!(canvas, img)
-        sleep(0.001)
+        sleep(0)
     end
-
-    # Create a condition object
-    c = Condition()
-
-    # Get the window
-    win = i["gui"]["window"]
-
-    # Notify the condition object when the window closes
-    signal_connect(win, :destroy) do _
-        notify(c)
-    end
-
-    # Wait for the notification before proceeding ...
-    wait(c)
-    close(cam)
+    return cam
 end
 
 end # module
